dd9d421543646afb734b4f5e7ff78eb1d78620c0
Andiurne <andiurne@gmail.com>
2026-07-11 00:48:52 -0700

    fix toLua calls

--- lib/dispatchers.nix
+++ lib/dispatchers.nix
@@ -1,9 +1,9 @@
 {lua, toLua}:{
   window = method: (lua "hl.dsp.window.${method}()");
-  windowArgs = method: args: (lua "hl.dsp.window.${method}(${toLua args})");
+  windowArgs = method: args: (lua "hl.dsp.window.${method}(${toLua {} args})");
   layout = name: (lua "hl.dsp.layout('${name}')");
   focus = rule: (lua "hl.dsp.focus(${rule})");
-  workspaceArgs = method: args: (lua "hl.dsp.workspace.${method}(${toLua args})");
+  workspaceArgs = method: args: (lua "hl.dsp.workspace.${method}(${toLua {} args})");
   dspCall = method: (lua "hl.dsp.${method}()");
-  dspCallArgs = method: args: (lua "hl.dsp.${method}(${toLua args})");
+  dspCallArgs = method: args: (lua "hl.dsp.${method}(${toLua {} args})");
 }
