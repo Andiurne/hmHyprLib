240e1a5cb48fea94cf193721da8e2f48af3bd9d0
Andiurne <andiurne@gmail.com>
2026-07-11 01:12:13 -0700

    fix toLua calls again

--- lib/dispatchers.nix
+++ lib/dispatchers.nix
@@ -1,9 +1,14 @@
-{lua, toLua}:{
+{lua, toLua}:
+let
+# Lua-fies sets into tables, leaves string literals alone
+checkArgs = args: if builtins.typeOf args == "string" then args else toLua {} args;
+in
+{
   window = method: (lua "hl.dsp.window.${method}()");
-  windowArgs = method: args: (lua "hl.dsp.window.${method}(${toLua {} args})");
+  windowArgs = method: args: (lua "hl.dsp.window.${method}(${checkArgs args})");
   layout = name: (lua "hl.dsp.layout('${name}')");
-  focus = rule: (lua "hl.dsp.focus(${toLua {} rule})");
-  workspaceArgs = method: args: (lua "hl.dsp.workspace.${method}(${toLua {} args})");
+  focus = rule: (lua "hl.dsp.focus(${checkArgs rule})");
+  workspaceArgs = method: args: (lua "hl.dsp.workspace.${method}(${checkArgs args})");
   dspCall = method: (lua "hl.dsp.${method}()");
-  dspCallArgs = method: args: (lua "hl.dsp.${method}(${toLua {} args})");
+  dspCallArgs = method: args: (lua "hl.dsp.${method}(${checkArgs args})");
 }
