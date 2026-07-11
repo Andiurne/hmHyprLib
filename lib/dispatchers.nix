1b269cc4e038841319451b153734fe00fb9f0d2b
Andiurne <andiurne@gmail.com>
2026-07-11 00:56:08 -0700

    add attrSet -> lua table support for focus

--- lib/dispatchers.nix
+++ lib/dispatchers.nix
@@ -2,7 +2,7 @@
   window = method: (lua "hl.dsp.window.${method}()");
   windowArgs = method: args: (lua "hl.dsp.window.${method}(${toLua {} args})");
   layout = name: (lua "hl.dsp.layout('${name}')");
-  focus = rule: (lua "hl.dsp.focus(${rule})");
+  focus = rule: (lua "hl.dsp.focus(${toLua {} rule})");
   workspaceArgs = method: args: (lua "hl.dsp.workspace.${method}(${toLua {} args})");
   dspCall = method: (lua "hl.dsp.${method}()");
   dspCallArgs = method: args: (lua "hl.dsp.${method}(${toLua {} args})");
