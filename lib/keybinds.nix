90135ce494e15b2fd208d869080d63b19a77220e
rickyrnt <rick.yarnot.255@gmail.com>
2026-07-10 23:52:06 -0700

    Nixify syntax and remove distincion between mod and key

--- lib/keybinds.nix
+++ lib/keybinds.nix
@@ -1,47 +1,39 @@
 {lua}:
 rec {
-bindVarParse = mod: key: (mod + " .. \" + " + key + "\"");
+	exec = cmd: (lua ("hl.dsp.exec_cmd('${cmd}')"));
 
-exec = cmd: (lua ("hl.dsp.exec_cmd(\"" + cmd + "\")"));
+	addFlags = bindSet: flagSet: bindSet // {_args = bindSet._args ++ [flagSet];};
 
-addFlags = bindSet: flagSet: bindSet // {_args = bindSet._args ++ [flagSet];};
-
-bindMap = mod: key:
-	if (mod == "mainMod") || (mod == "subMod") then
-		lua (bindVarParse mod key)
-	else
-		mod;
-
-simpleBind = mod: key: cmd: {
-	_args = [
-		(bindMap mod key)
-		(exec cmd)
-	];
-};
-pvarBind = mod: key: luaVar: {
-	_args = [
-		(bindMap mod key)
-		(lua("hl.dsp.exec_cmd(\"app2unit -- \" .. " + luaVar + ")"))
-	];
-};
-ipcBind = mod: key: cmd: {
-	_args = [
-		(bindMap mod key)
-		(lua ("hl.dsp.exec_cmd(ipc .. \"" + cmd + "\")"))
-	];
-};
-dspBind = mod: key: dsp: {
-	_args = [
-		(bindMap mod key)
-		dsp
-	];
-};
-fullBind = mod: key: dsp: rule:
-{
-	_args = [
-		(bindMap mod key)
-		dsp
-		rule
-	];
-};
+	simpleBind = mod: cmd: {
+		_args = [
+			mod
+			(exec cmd)
+		];
+	};
+	pvarBind = mod: luaVar: {
+		_args = [
+			mod
+			(exec "app2unit -- ${luaVar}")
+		];
+	};
+	ipcBind = mod: cmd: {
+		_args = [
+			mod
+			(exec "ipc ${cmd}")
+		];
+	};
+	dspBind = mod: dsp: {
+		_args = [
+			mod
+			dsp
+		];
+	};
+	fullBind = mod: dsp: flags:
+	{
+		_args = [
+			mod
+			dsp
+			flags
+		];
+	};
 }
